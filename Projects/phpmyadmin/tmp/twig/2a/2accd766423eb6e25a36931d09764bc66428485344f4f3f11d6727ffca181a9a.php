e3c32ca1735830e4e55fdc67324e5984605bd1f3
vipul82agravat <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>
2022-12-31 19:45:08 +0530

    add mysql project in dev repo

---
+++ Projects/phpmyadmin/tmp/twig/2a/2accd766423eb6e25a36931d09764bc66428485344f4f3f11d6727ffca181a9a.php
@@ -0,0 +1,71 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* database/import/index.twig */
+class __TwigTemplate_d9c2e75b5d32631dc7c3c0595882d6b62d99f700c92122e4d2c787092bfebca3 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "import.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("import.twig", "database/import/index.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo twig_escape_filter($this->env, twig_sprintf(_gettext("Importing into the database \"%s\""), ($context["db"] ?? null)), "html", null, true);
+    }
+
+    public function getTemplateName()
+    {
+        return "database/import/index.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  46 => 3,  35 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "database/import/index.twig", "/home/darshit/phpmyadmin/templates/database/import/index.twig");
+    }
+}
